5643101873f0c9437e326932ebf216a0f3becf7a
LvanArkel <lvanarkel@hotmail.nl>
2024-01-20 17:18:57 +0100

    hotfix depth

--- Assignment2Wavefront.cpp
+++ Assignment2Wavefront.cpp
@@ -229,7 +229,7 @@ void Assignment2WavefrontApp::Tick( float deltaTime )
 	
 	activeRays = SCRWIDTH * SCRHEIGHT * SAMPLES_PER_PIXEL;
 	//const int lowerRayBound = activeRays / 100;
-	const int maxDepth = 10;
+	const int maxDepth = 1;
 
 	generateKernel->Run(activeRays);
 
